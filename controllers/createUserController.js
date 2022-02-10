bcc92f57431b08c2768c3631efa87cea6f6aa312
BLACK <blackkitten010@gmail.com>
2022-02-11 03:20:17 +0700

    CheckNull

--- controllers/createUserController.js
+++ controllers/createUserController.js
@@ -1,6 +1,13 @@
+const bcrypt = require('bcrypt')
 const {createUserService} = require("../services")
 
 module.exports = async function createUserController(req, res){
-    await createUserService(req.body)
+    const {userName, userLastname, userEmail, userPassword} = req.body
+    if (!userName || !userLastname || !userEmail || !userPassword){
+        return res.send({message: "กรุณากรอกข้อมูลให้ครบถ้วน"})
+    }
+    const passwordHash = bcrypt.hashSync(userPassword, 10)
+    const data = {userName, userLastname, userEmail, userPassword:passwordHash}
+    await createUserService(data)
     return res.send(200) //200 == OK
 }
--- services/createUserService.js
+++ services/createUserService.js
@@ -1,14 +1,6 @@
-const bcrypt = require('bcrypt')
 const {UserModel} = require("../models")
 
 module.exports = async function createUserService(data){
-    const {userName, userLastname, userEmail, userPassword} = data
-    const passwordHash = bcrypt.hashSync(userPassword, 10)
-    const User = new UserModel({
-        userName,
-        userLastname,
-        userEmail,
-        userPassword:passwordHash
-    })
+    const User = new UserModel(data)
     return User.save()
 }
